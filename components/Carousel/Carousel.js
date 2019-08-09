94fab19a66b239d261e2c85242fe466a82df01e4
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-08-09 13:55:20 -0400

    completed mvp

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -18,3 +18,77 @@
   </div>
 */
 
+/*console.log("carousel is here");
+
+let carouselContainer = document.querySelector(".carousel-container");
+carouselContainer.appendChild(carouselCreator());
+
+function carouselCreator() {
+
+  const divCarousel = document.createElement("div");
+  divCarousel.classList.add("carousel");
+
+  const divLeftButton = document.createElement("div");
+  divLeftButton.classList.add("left-button");
+  divLeftButton.textContent = "<";
+
+  const mountainsImage = document.createElement("img");
+  mountainsImage.src = "./assets/carousel/mountains.jpeg";
+
+  const computerImage = document.createElement("img");
+  computerImage.src = "./assets/carousel/computer.jpeg";
+
+  const treesImage = document.createElement("img");
+  treesImage.src = "./assets/carousel/trees.jpeg";
+
+  const turntableImage = document.createElement("img");
+  turntableImage.src = "./assets/carousel/turntable.jpeg";
+
+  const divRightButton = document.createElement("div");
+  divRightButton.classList.add("right-button");
+  divLeftButton.textContent = ">";
+
+  divCarousel.appendChild(divLeftButton);
+  divCarousel.appendChild(mountainsImage);
+  divCarousel.appendChild(computerImage);
+  divCarousel.appendChild(treesImage);
+  divCarousel.appendChild(turntableImage);
+  divCarousel.appendChild(divRightButton);  
+
+  function navigate(direction) {
+
+    var counter = 0;
+   
+    var items = [mountainsImage, computerImage, treesImage, turntableImage];    
+
+    var amount = items.length;  
+    var current = items[0];
+
+    
+    counter = counter + direction;
+    if (direction === -1 && 
+        counter < 0) { 
+      counter = amount - 1; 
+    }
+    if (direction === 1 && 
+        !items[counter]) { 
+      counter = 0;
+    }
+    current = items[counter];
+    
+  }
+
+  
+  divRightButton.addEventListener('click', function(ev) {
+    navigate(1);
+  });
+  divLeftButton.addEventListener('click', function(ev) {
+    navigate(-1);
+  });
+  navigate(0);
+
+  return divCarousel;
+
+}*/
+
+
